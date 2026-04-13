cd55f916ac3c05bbddc238821a714d0f199a0f2b
君惜 <xinlei3166@126.com>
2026-04-14 03:22:42 +0800

    feat: 添加主题管理功能，包含主题接口和默认主题设置

---
+++ src/render/hooks/theme.ts
@@ -0,0 +1,32 @@
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+
+export interface Theme {
+  theme: 'dark' | 'light'
+  layout: 'side' | 'mix'
+  mode: 'vertical' | 'inline' | 'horizontal'
+  width: string
+  height: string
+  collapsed: boolean
+  collapsedWidth: string
+  headerTheme: boolean
+  showBreadcrumb: boolean
+  brandTheme: string
+}
+
+const theme = ref<Theme>({
+  theme: 'light', // light, dark
+  layout: 'mix', // side, mix
+  mode: 'inline',
+  width: '240px',
+  height: '64px',
+  collapsed: false,
+  collapsedWidth: '80px',
+  headerTheme: false,
+  showBreadcrumb: true, // 是否显示面包屑
+  brandTheme: '#0077fa'
+})
+
+export const useTheme = function (): Ref<Theme> {
+  return theme
+}
